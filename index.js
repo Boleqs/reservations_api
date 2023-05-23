37d9425d822a50d8687f40a6288e4272b462fb56
boleqs <boleqs@gmail.com>
2023-05-23 15:16:05 +0200

    V1.2

--- index.js
+++ index.js
@@ -12,11 +12,21 @@ const vehicule = vehicules.find(vehicule => vehicule.id === id)
 res.status(200).json(vehicule)
 })
 
+app.get('/reservations', (req,res) => {res.status(200).json(reservations)})
+
+app.get('/reservations/:idres', (req, res) => {
+const idres = parseInt(req.params.idres)
+const reservation = reservations.find(reservation => reservation.idres === idres)
+res.status(200).json(reservation)
+})
+
 app.post('/reservations', (req, res) => {
 console.log(req.body)
 reservations.push(req.body)
 res.status(200).json(reservations)
 })
+
+
 
 app.delete('/vehicules/:id', (req, res) => {
 const id = parseInt(req.params.id)
